4d2a55703c174c8ade7659e9898194d03ecce988
John Oliver <jroliver@madisoncollege.edu>
2021-03-02 05:39:48 -0600

    Added new entities for new tables.

---
+++ src/main/java/com/invoiceMaker/entity/ShippingAccountCarrier.java
@@ -0,0 +1,19 @@
+package com.invoiceMaker.entity;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Represents an Order that can have one or both
+ * a shipment and/or an invoice.
+ *
+ * @author John Oliver
+ */
+@Entity(name = "ShippingAccountCarrier")
+@Table(name = "Shipping_Account_Carrier")
+@Getter
+@Setter
+public class ShippingAccountCarrier {
+}
---
+++ src/main/java/com/invoiceMaker/entity/UserRoles.java
@@ -0,0 +1,20 @@
+package com.invoiceMaker.entity;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+
+/**
+ * Represents an
+ *
+ *
+ * @author John Oliver
+ */
+@Entity(name = "UserRoles")
+@Table(name = "User_Roles")
+@Getter
+@Setter
+public class UserRoles {
+}
